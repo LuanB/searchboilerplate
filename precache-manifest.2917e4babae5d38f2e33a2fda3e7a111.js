9607b32933eea08a318c8a7375950688e883f10d
Luan <luan.b@stjamesperth.com.au>
2019-07-12 23:31:56 +0800

    Updates

---
+++ precache-manifest.2917e4babae5d38f2e33a2fda3e7a111.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42c3eb7cf343ee8ea20543c8cee7aec",
+    "url": "/searchboilerplate/index.html"
+  },
+  {
+    "revision": "aa6819eeeb511444fdfb",
+    "url": "/searchboilerplate/static/css/main.1e7b9daa.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/searchboilerplate/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "aa6819eeeb511444fdfb",
+    "url": "/searchboilerplate/static/js/main.5a10c4e3.chunk.js"
+  },
+  {
+    "revision": "90c5b4003715de3a1087",
+    "url": "/searchboilerplate/static/js/runtime~main.98f7c1f5.js"
+  }
+]);
